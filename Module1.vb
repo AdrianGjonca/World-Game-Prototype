1cf6e3a31dea8570fe4927ba5c5ee6b717d77cdf
AdrianGjonca <55891807+AdrianGjonca@users.noreply.github.com>
2019-09-27 20:17:31 +0100

    Colisions

--- Module1.vb
+++ Module1.vb
@@ -2,7 +2,7 @@
     Dim playerX As Integer = 0
     Dim playerY As Integer = 0
     Dim screen(40, 40) As Char
-    Dim world(10, 10) As Char
+    Dim world(1024, 1024) As Char
     Sub Render()
         'Clear Screan
         For x As Integer = 0 To 40
@@ -12,10 +12,10 @@
         Next
 
         'Draw World
-        For x As Integer = 0 To 10
-            For y As Integer = 0 To 10
-                Dim sx As Integer = x - playerX
-                Dim sy As Integer = y + playerY
+        For x As Integer = 0 To 1024
+            For y As Integer = 0 To 1024
+                Dim sx As Integer = x - playerX + 20
+                Dim sy As Integer = -y + playerY + 20
 
                 If sx > 0 And sx < 40 And sy > 0 And sy < 40 Then
                     screen(sx, sy) = world(x, y)
@@ -41,7 +41,6 @@
                 data &= screen(x, y)
             Next
             data &= vbCrLf
-
         Next
         Console.WriteLine(data)
         Console.WriteLine(playerX & " " & playerY)
@@ -50,21 +49,36 @@
         Dim key As Char = Console.ReadKey.KeyChar
         Select Case key
             Case Is = "a"
-                playerX -= 1
+                If world(playerX - 2, playerY) = " " Then
+                    playerX -= 1
+                End If
             Case Is = "d"
-                playerX += 1
+                If world(playerX + 1, playerY) = " " Then
+                    playerX += 1
+                End If
             Case Is = "w"
-                playerY += 1
+                If world(playerX, playerY + 1) = " " Then
+                    playerY += 1
+                End If
             Case Is = "s"
-                playerY -= 1
+                If world(playerX, playerY - 2) = " " Then
+                    playerY -= 1
+                End If
         End Select
     End Sub
     Sub Main()
-        For x As Integer = 0 To 10
-            For y As Integer = 0 To 10
+        For x As Integer = 0 To 1024
+            For y As Integer = 0 To 1024
+                world(x, y) = " "
+            Next
+        Next
+        For x As Integer = 500 To 520
+            For y As Integer = 500 To 520
                 world(x, y) = "*"
             Next
         Next
+        playerY = 490
+        playerX = 490
 a:
         Render()
         TakeTurn()
