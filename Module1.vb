0c1d8eb4057720a89abb386dcff1c24c458612b9
AdrianGjonca <55891807+AdrianGjonca@users.noreply.github.com>
2019-09-27 19:44:56 +0100

    Initial

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Dim screen(40, 40) As Char
+    Sub Render()
+        Dim data As String = ""
+        Console.Clear()
+        For y As Integer = 0 To 40
+            For x As Integer = 0 To 40
+                data &= "*"
+                'Console.Write(screen(x, y))
+            Next
+            data &= vbCrLf
+
+        Next
+        Console.WriteLine(data)
+    End Sub
+    Sub Main()
+a:
+        Render()
+        Console.ReadKey()
+        GoTo a
+    End Sub
+
+End Module
